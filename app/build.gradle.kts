6093129d71567688d432164c7971715a873bb9eb
Msiol <54426779+Msiol@users.noreply.github.com>
2025-03-09 15:13:43 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,6 +95,41 @@ fun allCommitted(): Boolean {
     return stringBuilder.toString().isEmpty()
 }
 
+val keyProps = Properties()
+val keyPropsFile: File = rootProject.file("keystore/keystore.properties")
+keyProps.load(FileInputStream(keyPropsFile))
+fun getStoreFile(): String {
+    var storeFile = keyProps["storeFile"].toString()
+    if (storeFile.isEmpty()) {
+        storeFile = System.getenv("storeFile") ?: ""
+    }
+    return storeFile
+}
+
+fun getStorePassword(): String {
+    var storePassword = keyProps["storePassword"].toString()
+    if (storePassword.isEmpty()) {
+        storePassword = System.getenv("storePassword") ?: ""
+    }
+    return storePassword
+}
+
+fun getKeyAlias(): String {
+    var keyAlias = keyProps["keyAlias"].toString()
+    if (keyAlias.isEmpty()) {
+        keyAlias = System.getenv("keyAlias") ?: ""
+    }
+    return keyAlias
+}
+
+fun getKeyPassword(): String {
+    var keyPassword = keyProps["keyPassword"].toString()
+    if (keyPassword.isEmpty()) {
+        keyPassword = System.getenv("keyPassword") ?: ""
+    }
+    return keyPassword
+}
+
 android {
 
     namespace = "app.aaps"
@@ -149,6 +184,22 @@ android {
             manifestPlaceholders["appIcon"] = "@mipmap/ic_blueowl"
             manifestPlaceholders["appIconRound"] = "@mipmap/ic_blueowl"
         }
+    }
+
+signingConfigs {
+        create("release") {
+            storeFile = file(getStoreFile())
+            storePassword = getStorePassword()
+            keyAlias = getKeyAlias()
+            keyPassword = getKeyPassword()
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.findByName("release")
+        }
+
     }
 
     useLibrary("org.apache.http.legacy")
